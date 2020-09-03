7e17aac10e332bb67c3bf533f744e811874e610b
Sean <cd3k@Seans-MacBook-Pro.local>
2020-09-03 09:20:51 +0100

    added brand

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,10 +6,21 @@ const Wrapper = styled.div`
   height: 100vh;
 `
 
+const Image = styled.div`
+  background-image: url(/brand.png);
+  width: 100%;
+  height: 190px;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: -70px -20px;
+`
+
 function Layout({ children }) {
   return (
     <Wrapper>
-      <h1>Coin Catalogue</h1>
+
+      <Image />
+
       {children}
     </Wrapper>
   )
