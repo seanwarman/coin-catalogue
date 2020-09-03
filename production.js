25489cae83caac8c241a4e4adeab8460462d58df
Sean <cd3k@Seans-MacBook-Pro.local>
2020-09-03 09:25:26 +0100

    added production server

---
+++ production.js
@@ -0,0 +1,28 @@
+const express = require('express');
+// const favicon = require('express-favicon');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 8080;
+// app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(cors())
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+
+
+
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
